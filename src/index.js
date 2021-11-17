219f9e3507896d2f1f618e6f7547808fdc97695b
mcvaliente <maricruz.valiente@gmail.com>
2021-11-17 12:48:16 +0100

    Add MUI (React UI framework).

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./components/App";
+import { HashRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <HashRouter>
+    <React.StrictMode>
+      <CssBaseline />
+      <App />
+    </React.StrictMode>
+  </HashRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
