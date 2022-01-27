7dd578bf2506ec3b5a79953c1d2556b7ab8ce239
mcvaliente <maricruz.valiente@gmail.com>
2022-01-27 15:32:03 +0100

    Add date field validation.

--- src/utils/EnrollmentFieldValidation.js
+++ src/utils/EnrollmentFieldValidation.js
@@ -40,3 +40,45 @@ export function checkListField(fieldValue) {
     return true;
   }
 }
+
+export function checkDateField(fieldValue) {
+  if (typeof fieldValue !== undefined && fieldValue !== "") {
+    var isValid = isValidDate(fieldValue);
+    if (!isValid) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isValidDate(dateValue) {
+  try {
+    var inputDate = new Date(dateValue);
+    console.log("Input date: ", inputDate);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+export function greaterThanCurrentDate(dateValue) {
+  var inputDate = new Date(dateValue);
+  var currentDate = new Date();
+  if (inputDate > currentDate) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function greaterThanFirstDate(dateValue1, dateValue2) {
+  var inputDate1 = new Date(dateValue1);
+  var inputDate2 = new Date(dateValue2);
+  if (inputDate2 > inputDate1) {
+    return false;
+  } else {
+    return true;
+  }
+}
